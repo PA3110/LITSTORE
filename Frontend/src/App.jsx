3270f85678e7b322a80de21fc84793088d5741da
Prakriti Anand <prakritianand@gmail.com>
2025-02-24 07:30:30 +0530

    2nd_commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import Home from "./home/Home";
+import { Routes, Route } from "react-router-dom";
+import Courses from "./Courses/Courses";
 
 function App() {
   return (
-    
+    <>
+      {/* <Home />
+      <Course /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Courses />} />
+      </Routes>
+    </>
   );
 }
 
